95e5888886308a72dbe8ccb420493ef5cf3174be
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-15 21:22:08 +0800

    moved layout to be root component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Layout from "../components/common/Layout";
 import Carousel from "../components/homepage/Carousel";
 import Category from "../components/homepage/Category";
 import Hero from "../components/homepage/Hero";
@@ -6,11 +5,11 @@ import SlidingAds from "../components/homepage/SlidingAds";
 
 export default function Home() {
 	return (
-		<Layout>
+		<>
 			<SlidingAds />
 			<Category />
 			<Hero />
 			<Carousel />
-		</Layout>
+		</>
 	);
 }
