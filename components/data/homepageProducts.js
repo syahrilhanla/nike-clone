2d3889e5e6c2ebe2d29d7d350bd4d2415491ab67
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-15 21:51:42 +0800

    added price to productData

--- components/data/homepageProducts.js
+++ components/data/homepageProducts.js
@@ -3,51 +3,61 @@ const homepageProducts = [
 		productName: "Zion 2 PF",
 		productImg: "zion-2-pf.webp",
 		category: "Men's Basketball Shoes",
+		price: 149,
 	},
 	{
 		productName: "Kyrie Infinity EP",
 		productImg: "kyrie-infinity-ep.webp",
 		category: "Men's Basketball Shoes",
+		price: 189,
 	},
 	{
 		productName: "Lebron 19 Low",
 		productImg: "lebron-19-low.webp",
 		category: "Men's Basketball Shoes",
+		price: 199,
 	},
 	{
 		productName: "Nike BRSB",
 		productImg: "nike-brsb.webp",
 		category: "Men's Shoes",
+		price: 129,
 	},
 	{
 		productName: "Nike Phantom GT2 Academy MG",
 		productImg: "nike-phantom-gt2-academy-mg.webp",
 		category: "Turf Football Shoes",
+		price: 99,
 	},
 	{
 		productName: "Nike Phantom GT2 Academy TF",
 		productImg: "nike-phantom-gt2-academy-tf.webp",
 		category: "Turf Football Shoes",
+		price: 119,
 	},
 	{
 		productName: "Nike SB Zoom Blazer Low Pro GT",
 		productImg: "nike-sb-zoom-blazer-low-pro-gt.webp",
 		category: "Men's Shoes",
+		price: 129,
 	},
 	{
 		productName: "Nike Tiempo GT2 Legend Academy MG",
 		productImg: "nike-tiempo-gt2-legend-academy-mg.webp",
 		category: "Football Shoes",
+		price: 149,
 	},
 	{
 		productName: "Nike Tiempo GT2 Legend Academy TF",
 		productImg: "nike-tiempo-gt2-legend-academy-tf.webp",
 		category: "Turf Football Shoes",
+		price: 129,
 	},
 	{
 		productName: "Nike Zoom Lebron 3",
 		productImg: "nike-zoom-lebron-3.webp",
 		category: "Basketball Shoes",
+		price: 215,
 	},
 ];
 
